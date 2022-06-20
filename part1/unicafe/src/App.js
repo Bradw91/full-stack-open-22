20d0bc99657af4a8c090dd816a0be3c3012fd664
Bradw91 <imbrad91@gmail.com>
2022-06-20 20:50:17 +0200

    Added Statistics Component

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 import Button from './components/Button'
+import Statistics from './components/Statistics'
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allStats, setStats] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    average: 0,
-    positive: 0
-  })
+  //const [all, setAll] = useState(0)
 
   const handleGood = () => {
     setGood(good+1)
@@ -29,6 +24,24 @@ const App = () => {
     console.log(neutral)
   }
 
+  const countAll = (good, bad, neutral) => {
+    let result = good + bad + neutral
+    return result
+  }
+  // good = -1 neutral = 0 bad = -1
+  // const calcAverage = (good, neutral, bad) => {
+  //   let average = 0
+
+  // }
+  
+  const percentPositive = (all, good) => {
+    let result = Math.round((good / all) * 100,2);
+    if (isNaN(result)){
+      return ""
+    }
+    return result + "%"
+  }
+
 
   return (
     <div>
@@ -36,6 +49,7 @@ const App = () => {
       <Button text='Good' onClick={handleGood}/>
       <Button text='Neutral' onClick={handleNeutral}/>
       <Button text='Bad' onClick={handleBad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={countAll(good, neutral, bad)}  positive={percentPositive(countAll(good, neutral,bad), good)}/>
     </div>
   )
 }
---
+++ part1/unicafe/src/components/Statistics.js
@@ -0,0 +1,22 @@
+const Statistics = ({ good, bad, neutral, all, positive }) => {
+
+  if (all === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Percent Positive: {positive}</p>
+    </div>
+  )
+}
+
+export default Statistics
