8b683a6ad5e96a3be8cb911dbe0052e6aef31e06
Bradw91 <imbrad91@gmail.com>
2022-06-22 13:35:01 +0200

    Reduce function for calculating Total

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,12 +1,11 @@
-const Total = (props) => {
+const Total = ({ parts }) => {
+  const total = parts.reduce(
+    (prev, currentValue) => prev + currentValue.exercises,
+    0
+  );
   return (
     <>
-      <p>
-        Number of exercises:{" "}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
-      </p>
+      <p>Number of exercises: {total}</p>
     </>
   );
 };
